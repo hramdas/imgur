b7a83135b3579d81f8e0e0e7af28f85c64ea95c7
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-01-29 14:51:26 +0530

    newapi map

--- components/data.js
+++ components/data.js
@@ -1,5 +1,5 @@
 // var myHeaders = new Headers();
-// myHeaders.append("Authorization", "563492ad6f91700001000001e9a97995130c470ea0cf68687f57f0b0");
+// myHeaders.append("Authorization", "");
 
 // function data() {
 //   fetch("https://api.pexels.com/v1/curated/?page=1",{
