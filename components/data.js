bfbfe5315b9cd7ef4f7408b990647f29268f9ec3
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-01-29 09:19:10 +0530

    data fetch

---
+++ components/data.js
@@ -0,0 +1,10 @@
+// function data() {
+//   fetch("https://api.pexels.com/v1/curated/?page=3")
+//     .then((response) => response.text())
+//     .then((result) => {
+//       return result;
+//     })
+//     .catch((error) => console.log("error", error));
+// }
+
+// export default data;
