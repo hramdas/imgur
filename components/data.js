580ac651838658c4c741d3a6746943983c063068
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-01-29 14:50:56 +0530

    newapi map

--- components/data.js
+++ components/data.js
@@ -1,10 +1,19 @@
+// var myHeaders = new Headers();
+// myHeaders.append("Authorization", "563492ad6f91700001000001e9a97995130c470ea0cf68687f57f0b0");
+
 // function data() {
-//   fetch("https://api.pexels.com/v1/curated/?page=3")
+//   fetch("https://api.pexels.com/v1/curated/?page=1",{
+//     method: "GET",
+//     headers: myHeaders,
+//   })
 //     .then((response) => response.text())
 //     .then((result) => {
-//       return result;
+//         console.log(result)
+//      // return result;
 //     })
 //     .catch((error) => console.log("error", error));
 // }
+// data() 
+
 
 // export default data;
--- index.js
+++ index.js
@@ -1,50 +1,48 @@
 var page = 1;
 var size = 10
 
-const getPost = ()=>{
-    fetch(`https://api.pexels.com/v1/curated/?page=${page}&per_page=${size}`)
-  .then((response) => response.json())
-  .then((result) => {
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "");
+
+function getdata() {
+  fetch("https://api.pexels.com/v1/curated/?page=1",{
+    method: "GET",
+    headers: myHeaders,
+  })
+    .then((response) => response.json())
+    .then((result) => {
+       
     result.photos.map((e) => {
-        console.log(e)
     var itemslist = document.getElementById("itemslist");
-    //   let itemdiv = document.createElement("div");
-    //   let itemimg = document.createElement("img");
-    //   let itemp = document.createElement("p");
-    //   let act = document.createElement("div");
+    console.log(e)
+        var datamap = `<div id="item">
+                        <img
+                        id="contimg"
+                        src="${e.src.medium}"
+                        alt="${e.alt}"
+                        />
+                        <p>${e.alt}</p>
+                        <div id="contActivity">
+                            <span>
+                                <i class="fas fa-arrow-up"></i>
+                                <span>200</span>
+                                <i class="fas fa-arrow-down"></i>
+                            </span>
+                            <span>
+                                <i class="far fa-comment-alt"></i>
+                                <span></span>
+                            </span>
+                            <span>
+                                <i class="fas fa-eye"></i>
+                                <span>300</span>
+                            </span>
+                        </div>
+                    </div>`
 
-    //   act.classList.add("contActivity");
-    //   itemdiv.classList.add("item");
+        itemslist.insertAdjacentHTML('beforeend', datamap)
+        });
 
-    //   itemimg.src = e.src.medium;
-    //   itemdiv.append(itemimg);
-
-    var datamap = `<div id="item">
-                    <img
-                    id="contimg"
-                    src="${e.src.medium}"
-                    alt="dog"
-                    />
-                    <p>May I Introduce you all too Beauregard</p>
-                    <div id="contActivity">
-                        <span>
-                            <i class="fas fa-arrow-up"></i>
-                            <span>200</span>
-                            <i class="fas fa-arrow-down"></i>
-                        </span>
-                        <span>
-                            <i class="far fa-comment-alt"></i>
-                            <span>17</span>
-                        </span>
-                        <span>
-                            <i class="fas fa-eye"></i>
-                            <span>300</span>
-                        </span>
-                    </div>
-                </div>`
-      itemslist.insertAdjacentHTML('afterend') = datamap
-    });
   })
   .catch((error) => console.log("error", error));
 }
-getPost()
+getdata()
