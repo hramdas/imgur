d02788e48c7ef954e47530b3e9536e21d4952101
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-01-30 18:50:46 +0530

    data api

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 var page = 1;
-var size = 10
+var size = 8;
 
 var myHeaders = new Headers();
-myHeaders.append("Authorization", "");
+myHeaders.append(
+  "Authorization",
+  ""
+);
 
 function getdata() {
-  fetch("https://api.pexels.com/v1/curated/?page=1",{
+  fetch(`https://api.pexels.com/v1/curated/?page=${page}&per_page=${size}`, {
     method: "GET",
-    headers: myHeaders,
+    headers: myHeaders
   })
     .then((response) => response.json())
     .then((result) => {
-       
-    result.photos.map((e) => {
-    var itemslist = document.getElementById("itemslist");
-    console.log(e)
+      result.photos.map((e) => {
+        var itemslist = document.getElementById("itemslist");
+        console.log(e);
         var datamap = `<div id="item">
                         <img
                         id="contimg"
@@ -24,25 +26,14 @@ function getdata() {
                         <p>${e.alt}</p>
                         <div id="contActivity">
                             <span>
-                                <i class="fas fa-arrow-up"></i>
-                                <span>200</span>
-                                <i class="fas fa-arrow-down"></i>
-                            </span>
-                            <span>
-                                <i class="far fa-comment-alt"></i>
-                                <span></span>
-                            </span>
-                            <span>
-                                <i class="fas fa-eye"></i>
-                                <span>300</span>
+                            Photographer : <b> ${e.photographer}<b/>
                             </span>
                         </div>
-                    </div>`
+                    </div>`;
 
-        itemslist.insertAdjacentHTML('beforeend', datamap)
-        });
-
-  })
-  .catch((error) => console.log("error", error));
+        itemslist.insertAdjacentHTML("beforeend", datamap);
+      });
+    })
+    .catch((error) => console.log("error", error));
 }
-getdata()
+getdata();
